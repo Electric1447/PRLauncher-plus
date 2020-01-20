3fa7f6e47a42776fdbf32492b9ab146b6f5d835b
Electric <itai.levin4@gmail.com>
2020-01-20 20:00:39 +0200

    Add project files.

---
+++ PRLauncher-plus/MainForm.cs
@@ -0,0 +1,157 @@
+﻿using PRLauncher_plus.Objects;
+using PRLauncher_plus.Properties;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PRLauncher_plus {
+    public partial class MainForm : Form {
+
+        WadList iwads = new WadList();
+        Levels levelList;
+
+        string folderPath = "";
+
+        static string[] prboomExec = new string[] {"prboom-plus.exe", "glboom-plus.exe"};
+        static string[] prboomExecNames = new string[] {"PRBoom+ (" + prboomExec[0] + ")", "GLBoom+ (" + prboomExec[1] + ")"};
+
+        static string[] complevels = new string[] {"-1 - Current Prboom-plus", "0 - Doom v1.2", "1 - Doom v1.666", "2 - Doom v1.9", "3 - Ultimate Doom", "4 - Final Doom's doom2.exe", "5 - DOSDoom", "6 - TASDDOOM",
+            "7 - Boom's compatibility mode", "8 - Boom v2.01", "9 - Boom v2.02", "10 - LxDoom v1.4.x", "11 - MBF", "12 - PrBoom v2.03beta", "13 - PrBoom v2.1.0", "14 - PrBoom v2.1.1-2.2.6",
+            "15 - PrBoom v2.3.x", "16 - PrBoom v2.4.0", "17 - Latest PrBoom-plus"};
+
+        static string[] difficulties = new string[] {"None (select ingame)", "I'm too young to die", "Hey, not too rough", "Hurt me plenty", "Ultra-Violence", "Nightmare!"};
+
+        int cExecutable = 0, cIWad = 0, cComplevel = 0, cDifficulty = 0;
+
+        public MainForm() {
+            InitializeComponent();
+
+            cExecutable = Settings.Default.cExecutablePref;
+            cIWad = Settings.Default.cIWadPref;
+            cComplevel = Settings.Default.cComplevelPref;
+            cDifficulty = Settings.Default.cDifficultyPref;
+            folderPath = Settings.Default.folderPathPref;
+
+            dirTextBox.Text = folderPath;
+            levelTextBox.Text = Settings.Default.warpPref;
+
+            compComboBox.Items.AddRange(complevels);
+            compComboBox.SelectedIndex = cComplevel;
+            diffComboBox.Items.AddRange(difficulties);
+            diffComboBox.SelectedIndex = cDifficulty;
+
+            if (folderPath != "")
+                iwads.detectWads(folderPath);
+
+            RefreshLevelList();
+
+            if (cExecutable == 0)
+                execRadio1.Checked = true;
+            if (cExecutable == 1)
+                execRadio2.Checked = true;
+
+        }
+
+        private void run (object sender, EventArgs e) {
+
+            string diff_temp = "", warp_temp = levelTextBox.Text;
+
+            if (cDifficulty != 0)
+                diff_temp = " -skill " + cDifficulty;
+
+            if (warp_temp != "")
+                warp_temp = " -warp " + warp_temp;
+
+            if (!folderPath.Equals("")) {
+                try {
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    startInfo.FileName = folderPath + @"\" + prboomExec[cExecutable];
+                    startInfo.Arguments = " -iwad " + iwads.getWadFilename(cIWad) + " -complevel " + (cComplevel - 1) + warp_temp + " " + diff_temp + " " + argTextBox.Text;
+                    Process.Start(startInfo);
+                } catch { Console.WriteLine("ERROR: can't open executable"); }
+            }
+
+            Settings.Default.warpPref = levelTextBox.Text;
+            Settings.Default.Save();
+        }
+
+        private void ChooseDirectoryButton (object sender, EventArgs e) {
+
+            FolderBrowserDialog folderBD = new FolderBrowserDialog();
+
+            if (folderBD.ShowDialog() == DialogResult.OK) {
+                folderPath = folderBD.SelectedPath;
+                Settings.Default.folderPathPref = folderPath;
+                dirTextBox.Text = folderPath;
+            }
+
+            Settings.Default.Save();
+        }
+
+        private void RefreshDirectory (object sender, EventArgs e) {
+
+            folderPath = dirTextBox.Text;
+            Settings.Default.folderPathPref = folderPath;
+            Settings.Default.Save();
+
+            if (Directory.Exists(dirTextBox.Text) && (File.Exists(dirTextBox.Text + @"\" + prboomExec[0]) || File.Exists(dirTextBox.Text + @"\" + prboomExec[1]))) {
+
+                iwads.detectWads(folderPath);
+                iwadComboBox.Items.Clear();
+                iwadComboBox.Items.AddRange(iwads.getWadsFullTitle());
+
+                if (iwads.getWadsFilename().Length <= cIWad) {
+                    cIWad = 0;
+                    Settings.Default.cIWadPref = cIWad;
+                }
+
+                iwadComboBox.SelectedIndex = cIWad;
+
+                
+            } else {
+                iwadComboBox.Items.Clear();
+            }
+
+        }
+
+        private void ExecutableChanged (object sender, EventArgs e) {
+
+            if (execRadio1.Checked)
+                cExecutable = 0;
+            if (execRadio2.Checked)
+                cExecutable = 1;
+
+            Settings.Default.cExecutablePref = cExecutable;
+            Settings.Default.Save();
+
+        }
+
+        private void iwadCB_IndexChanged (object sender, EventArgs e) {
+            cIWad = iwadComboBox.SelectedIndex;
+            RefreshLevelList();
+            Settings.Default.cIWadPref = cIWad;
+            Settings.Default.Save();
+        }
+
+        private void compCB_IndexChanged (object sender, EventArgs e) {
+            cComplevel = compComboBox.SelectedIndex;
+            Settings.Default.cComplevelPref = cComplevel;
+            Settings.Default.Save();
+        }
+
+        private void diffCB_IndexChanged (object sender, EventArgs e) {
+            cDifficulty = diffComboBox.SelectedIndex;
+            Settings.Default.cDifficultyPref = cDifficulty;
+            Settings.Default.Save();
+        }
+
+        private void RefreshLevelList () {
+            levelComboBox.Items.Clear();
+            if (iwads.IsKnown(cIWad) && folderPath != "") {
+                levelList = new Levels(iwads.getWadFilename(cIWad));
+                levelComboBox.Items.AddRange(levelList.GetLevelList());
+            }
+        }
+    }
+}
---
+++ PRLauncher-plus/Objects/WadList.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRLauncher_plus {
+
+    class WadList {
+
+        private readonly string[] wadsFilenameDB = new string[] { "doom.wad", "doom1.wad", "doomu.wad", "doom2.wad", "plutonia.wad", "tnt.wad"};
+        private readonly string[] wadsTitleDB = new string[] { "Doom / Ultimate Doom", "Doom", "The Ultimate Doom", "Doom II: Hell on Earth", "The Plutonia Experiment", "TNT: Evilution"};
+
+        private string[] wadsFilename;
+        private string[] wadsTitle;
+
+        public WadList () {
+
+        }
+
+        public void detectWads (string path) {
+
+            if (!Directory.Exists(path)) {
+                noIWads("Invalid path");
+                return;
+            }
+
+            string[] wads = Directory.GetFiles(path, "*.wad");
+
+            for (int i = 0; i < wads.Length; i++)
+                wads[i] = wads[i].Substring(wads[i].LastIndexOf('\\') + 1);
+
+            if (wads == null) {
+                noIWads("No iwads found");
+                return;
+            }
+
+            if (wads.Length == 0) {
+                noIWads("No iwads found");
+                return;
+            }
+
+            string[] tempWadFN = new string[wads.Length];
+            string[] tempWadT = new string[wads.Length];
+            
+            string[] tempStr = wads.Select(s => s.ToLowerInvariant()).ToArray();
+            Array.Sort(tempStr);
+
+            for (int i = 0; i < tempStr.Length; i++) {
+                if (Array.IndexOf(wadsFilenameDB, tempStr[i]) > -1) {
+                    int index = Array.IndexOf(wadsFilenameDB, tempStr[i]);
+                    tempWadFN[i] = wadsFilenameDB[index];
+                    tempWadT[i] = wadsTitleDB[index];
+                } else {
+                    tempWadFN[i] = tempStr[i];
+                    tempWadT[i] = tempStr[i];
+                }
+            }
+
+            this.setWadsFilename(tempWadFN);
+            this.setWadsTitle(tempWadT);
+        }
+
+        private void noIWads (string msg) {
+            this.wadsFilename = new string[] {""};
+            this.wadsTitle = new string[] {"No iwads"};
+            Console.WriteLine(msg);
+        }
+
+        // Get & Set methods
+        public string[] getWadsFilename () {
+            return this.wadsFilename;
+        }
+
+        public void setWadsFilename (string[] wfn) {
+            this.wadsFilename = wfn;
+        }
+
+        public string[] getWadsTitle () {
+            return this.wadsTitle;
+        }
+
+        public void setWadsTitle (string[] wt) {
+            this.wadsTitle = wt;
+        }
+
+        public string getWadFilename (int index) {
+            return this.wadsFilename[index];
+        }
+
+        public void setWadFilename (string wfn, int index) {
+            this.wadsFilename[index] = wfn;
+        }
+
+        public string getWadTitle (int index) {
+            return this.wadsTitle[index];
+        }
+
+        public void setWadTitle (string wt, int index) {
+            this.wadsTitle[index] = wt;
+        }
+
+        public string[] getWadsFullTitle () {
+            string[] str = new string[this.wadsFilename.Length];
+
+            for (int i = 0; i < str.Length; i++) {
+                if (this.IsKnown(i))
+                    str[i] = this.wadsTitle[i] + " (" + this.wadsFilename[i] + ")";
+                else
+                    str[i] = this.wadsTitle[i];
+            }
+
+            return str;
+        }
+
+        public string getWadFullTitle (int index) {
+            return (this.wadsTitle[index] + " (" + this.wadsFilename[index] + ")");
+        }
+
+        public bool IsKnown (int index) {
+
+            if (this.wadsFilename == null)
+                return false;
+
+            return (Array.IndexOf(wadsFilenameDB, this.wadsFilename[index]) > -1);
+        }
+
+    }
+}
